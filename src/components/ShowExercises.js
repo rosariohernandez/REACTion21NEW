edcd8b15cc6617431af2c9f78324673681fde75f
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-06-24 19:43:06 -0400

    login and workout CRUD

---
+++ src/components/ShowExercises.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+import Table from 'react-bootstrap/Table';
+
+
+function ShowExercises() {
+    useEffect(()=>{
+        fetchItems();
+    },[]);
+const [exercises, setItems] = useState([]);
+
+async function handleDelete(event){
+    event.preventDefault();
+    if (window.confirm("Are you sure you want to delete?")){
+    const url = 'http://localhost:5000/exercises/'+event.target.value;
+
+    await fetch(url, {
+        method: 'delete'
+
+      });   
+     
+      fetchItems();  
+    }  
+}
+    const fetchItems = async ()=>{
+        const data = await fetch('http://localhost:5000/exercises');
+        const exercises = await data.json();        
+        setItems(exercises);
+    }
+  return (
+    <div className="App">
+<Table striped bordered hover variant="light">
+  <thead className="thead-dark">
+    <tr>      
+      <th>Name</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+
+  <tbody>
+  {exercises.map(item =>(
+    
+    <tr>      
+      <td>{item.title}</td>     
+      <td><button className="btn btn-primary" value={item._id} onClick={handleDelete}>Delete</button></td>     
+    </tr>
+    ))} 
+  </tbody>
+</Table>       
+              
+    </div>
+  );
+}
+
+export default ShowExercises;
